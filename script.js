4a6dd588f4c0be9dd6df8ad0a7d5311ca9bc3d7c
Lucas Silva <lucastech@LAPTOP-JK0FL1VB.localdomain>
2022-08-17 21:15:13 -0300

    Add clouds e otimizando codigo

--- script.js
+++ script.js
@@ -1,43 +1,101 @@
 const $panda = document.querySelector('.panda')
-const $obt = document.querySelector('.obt')
+const $obstacle = document.querySelector('.obstacle')
 const $conteiner = document.querySelector('.conteiner')
-click = true
-   
-document.addEventListener('keydown', event =>{
-     if(click === true){
-        if(event.keyCode === 32){
-            $panda.classList.add('pandaAn')
-            setTimeout(() =>{
-            $panda.classList.remove('pandaAn')
-            },700) 
+let $contador = document.querySelector('.contador')
+let play = document.querySelector('.start')
+const $iniciar = document.querySelector('.pressStart')
+$obstacle.style.display = 'none'
+const $cloud = document.querySelector(".cloud")
+const $cloud002 = document.querySelector(".cloud002")
+$cloud.style.animation = 'none'
+$cloud002.style.animation = 'none'
+// inicia a animação do tronco quando o botão "Start" é clicado
+$iniciar.addEventListener('click', () => {
+    play.style.display = 'none'
+    $cloud.style.animation = 'cloudAnimation 4.9s linear infinite'
+    $cloud002.style.animation = 'cloudAnimation002 5.3s linear infinite'
+    $obstacle.style.display = 'block'
+    $obstacle.style.animation = 'obtAN 2.5s linear infinite'
+
+    // loop para verifica se o jogador perdeu
+    const loop = setInterval(() => {
+
+        const obstacle = $obstacle.offsetLeft;
+        const panda = +window.getComputedStyle($panda).bottom.replace('px', '')
+        let tamanhoTela = screen.height
+        
+        // verifica se a tela tem altura menor que 500px
+        if (tamanhoTela < 501) {
+            $obstacle.style.animation = 'obtAN 1.8s linear infinite'
+            // função para o panda pular com o click 
+            $conteiner.addEventListener('mousedown', () => {
+                $panda.classList.add('pandaAnimation')
+                setTimeout(() => {
+                    $panda.classList.remove('pandaAnimation')
+                }, 700)
+            })
+
+            // se for verdadeiro significa que o panda colidiu com o tronco finalizando as animações.
+            if (obstacle <= 163 && obstacle > 78 && panda < 60) {
+                $obstacle.style.animation = 'none';
+                $obstacle.style.left = `${obstacle}px`
+                $panda.style.animation = 'none'
+                $panda.style.bottom = `${panda}px`
+
+                // Finaliza os loops
+                clearInterval(loop)
+                clearInterval(contador)
+
+                // recarrega a pagina após 1.2 segundos para reiniciar o jogo
+                setTimeout(() => {
+                    document.location.reload()
+                }, 1200)
+
+            }
+
+        } else {
+            
+            
+            // Função para o panda pular quando preciona a tecla Space/Espaço
+            const pulopanda = document.addEventListener('keypress', event => {
+
+                if (event.key === " ") {
+                    $panda.classList.add('pandaAnimation')
+                    // função para remover animação do panda
+                    setTimeout(() => {
+                        $panda.classList.remove('pandaAnimation')
+                    }, 700)
+                }
+
+            })
+
+            // se for verdadeiro significa que o panda colidiu com o tronco finalizando as animações.
+            if (obstacle <= 210 && obstacle > 90 && panda < 83) {
+                $obstacle.style.animation = 'none';
+                $obstacle.style.left = `${obstacle}px`
+                $panda.style.animation = 'none'
+                $panda.style.bottom = `${panda}px`
+
+                // Finaliza os loops
+                clearInterval(loop)
+                clearInterval(contador)
+
+                // recarrega a pagina após 1.2 segundos para reiniciar o jogo
+                setTimeout(() => {
+                    document.location.reload()
+                }, 1200)
+
+
+            }
         }
-    
-    }
+    }, 50)
+    // Loop para contar pontos 
+    let pontos = 0
+    const contador = setInterval(() => {
+        pontos++
+        
+        $contador.textContent = `${pontos}`
 
-    setTimeout(() =>{
-        click = false
-        setTimeout(() => {
-            click = true
-        },700)
-    },0)
-})
-$conteiner.addEventListener('click', () =>{
-    $panda.classList.add('pandaAn')
-    setTimeout(() =>{
-    $panda.classList.remove('pandaAn')
-    },700)
-})
-const loop = setInterval(() =>{
-    const obt = $obt.offsetLeft;
-    const panda = +window.getComputedStyle($panda).bottom.replace('px', '')
-    console.log(obt)
-    
-    if(obt <= 210 && obt > 75 && panda < 50){
-        $obt.style.animation = 'none';
-        $obt.style.left = `${obt}px`
+    }, 50)
 
-        $panda.style.animation = 'none'
-        $panda.style.bottom = `${panda}px`
-        clearInterval(loop)
-    }
-}, 10)
+})
